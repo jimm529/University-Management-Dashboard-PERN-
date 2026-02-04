0895e632bfdbd115d1afff2b6f5b8bdd44980182
jimmy <jimmiparekh936@gmail.com>
2026-02-04 17:47:57 +0530

    Replaced project files with new copied structure

--- src/components/subjects/create.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const SubjectCreate = () => {
-    return (
-        <div>Create</div>
-    )
-}
-export default SubjectCreate;
--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type Subject ={
-    id:number;
-    name:string;
-    code:string;
-    description:string;
-    department:string;
-    createdAt:string;
-
-}
